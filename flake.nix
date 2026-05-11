46e94deb2c07b3fca02210001baf317c2c552034
Bojan Konjevic <konjevicbojan1@gmail.com>
2026-05-11 20:17:54 +0200

    made flake cd into src/

--- flake.nix
+++ flake.nix
@@ -22,14 +22,14 @@
           export UV_PYTHON="${python}/bin/python3"
           export PATH="${python}/bin:${pkgs.uv}/bin:$PATH"
           export UV_PROJECT_ENVIRONMENT="$HOME/.cache/zenit-venv"
-          export PYTHONPATH="$${self}/src:$${PYTHONPATH}"
+          cd "${self}/src"
           exec uv run --no-project \
             --with "jinja2" \
             --with "typer" \
             --with "pyyaml" \
             --with "tomli-w" \
             --with "tomlkit" \
-            python "${self}/src/scaffolder/main.py" "$$@"
+            python -m scaffolder.main "$@"
         '');
       };
 
